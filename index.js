e14c412d6776c5b76b5a71ca48dc6e810c0dfb18
SureNarendra341 <narendrasure2002@gmail.com>
2024-07-16 21:56:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let a="narendra"
+console.log(typeof a)
+
+let b=2
+console.log(typeof b)
+let bool=true
+console.log(typeof bool)
+let value
+console.log(typeof value)
+let c=null
+console.log(typeof c)
+
+let x=23
+let y=54
+// addition
+console.log("sum",x+y)
+// subraction
+console.log("sub",x-y)
+// multiplication
+console.log("multi",x*y)
+// divison
+console.log("div",x/y)
+// modulues
+console.log("mod",x%y)
+
+let i=20
+let j=45
+console.log(x==y)
+
+console.log(x===y)
+
+console.log(x!=y)
+
+console.log(x!==y)
+
+console.log(x>y)
+
+console.log(x<y)
+
+console.log(x>=y)
+
+console.log(x<=y)
